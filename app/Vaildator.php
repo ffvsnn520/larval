c3fd2c1507d0ef10c2dd249b6982172dd3cfc875
6362334@qq.com <6362334@qq.com>
2017-05-08 16:17:24 -0700

    上传验证类测试composer

---
+++ app/Vaildator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2017/5/8
+ * Time: 16:03
+ */
+
+namespace App;
+
+
+class Vaildator
+{
+    private $message = '';
+
+    public function validate()
+    {
+        echo '测试composer';
+    }
+}
